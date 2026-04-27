41a59f49a0128a8743ead2c85f5036fc356a6a17
Samus1906s <sacerdasce@est.utn.ac.cr>
2026-04-26 19:13:06 -0600

    Se crea el componente Navbar

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,24 @@
+export default function NavigationBar () {
+    return (
+       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+
+        <a className="navbar-brand" href="/">
+          Trivia App
+        </a>
+
+        /* Flata del button */
+
+        <div className="collapse navbar-collapse" id="navbarContent">
+
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Inicio</a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </nav>
+    );
+}
