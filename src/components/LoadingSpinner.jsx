c4272dc941bdc0b6c7c9f6e76057bd83c780854e
Samus1906s <sacerdasce@est.utn.ac.cr>
2026-04-26 20:45:13 -0600

    LoadingSpinner
    Componente que muestra una reuda de carga mientras se muestra la pregunta

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,10 @@
+export default function LoadingSpinner () {
+    return (
+        <div className="text-center mt-4">
+
+            <div className="spinner-border text-primary"></div>
+            <p>Cargando Pregunta...</p>
+            
+        </div>
+    );
+}
