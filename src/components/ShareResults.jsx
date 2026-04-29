0a5347ba391fca52b2f05dc937f9a8c3e813c4de
Samus1906s <sacerdasce@est.utn.ac.cr>
2026-04-28 23:18:55 -0600

    ShareResult
    Se hicieron alguna modificaciones en este componente con respecto a los metodos de compartir los resulatdos

--- src/components/ShareResults.jsx
+++ src/components/ShareResults.jsx
@@ -1,19 +1,43 @@
+import PropTypes from "prop-types";
 import Button from "./Button";
 
-export default function ShareResults ({puntos}) {
+export default function ShareResults({ puntos = 0 }) {
 
-    const compartir = () => {
-        alert(`Obtuve ${puntos} los puntos en el TriviaGame`)
+    const compartirGoogle = () => {
+        const asunto = "Resultado TriviaGame";
+        const mensaje = `Obtuve ${puntos} puntos en la TriviaGame que hice`;
+        const url = `https://mail.google.com/mail/?view=cm&fs=1&su=${encodeURIComponent(asunto)}&body=${encodeURIComponent(mensaje)}`;
+
+        window.open(url, "_blank");
     }
+
+    const compartirFacebook = () => {
+        const url = `https://www.facebook.com/sharer/sharer.php?u=https://mi-trivia.com&quote=Obtuve ${puntos} puntos en TriviaGame`
+        window.open(url, "_blank")
+    }
+
     return (
 
-        <div className="mt-3">
-      <Button
-        texto="Compartir resultado"
-        tipo="success"
-        onClick={compartir}
-      />
-    </div>
+        <div className="d-flex justify-content-center gap-2 flex-wrap">
+
+            <Button
+                texto="Compartir en Google"
+                tipo="danger"
+                onClick={compartirGoogle}
+            />
+
+            <Button
+                texto="Compartir en Facebook"
+                tipo="primary"
+                onClick={compartirFacebook}
+            />
+        </div>
+
+
 
     );
 }
+
+ShareResults.propTypes = {
+    puntos: PropTypes.number
+};
